e34a0bb40df4f566a7cff1d32287c0f4f15ce714
Pig Fang <g-plane@hotmail.com>
2021-08-15 09:51:28 +0800

    support parsing "catalog"

--- parser.ts
+++ parser.ts
@@ -49,12 +49,119 @@ export function* tokenize(source: string): TokenStream {
   return { pos: source.length, line: -1, column: -1, type: TokenType.EOF };
 }
 
-export function parse(source: string): CueSheeet {
-  for (const token of tokenize(stripBOM(source))) {
-    console.log(token)
+interface ParserState {
+  inTrack: boolean;
+}
+
+interface Context {
+  sheet: CueSheeet;
+
+  state: ParserState;
+
+  /**
+   * Raise a parsing error.
+   *
+   * @param message error message
+   * @param errorAt error location
+   */
+  raise(
+    message: string,
+    errorAt: { pos: number; line: number; column: number },
+  ): void;
+}
+
+function expectToken(
+  tokens: TokenStream,
+  type: TokenType.EOF,
+  context: Context,
+): TokenEOF;
+function expectToken(
+  tokens: TokenStream,
+  type: TokenType.LineBreak,
+  context: Context,
+): TokenLineBreak;
+function expectToken(
+  tokens: TokenStream,
+  type: TokenType.Unquoted,
+  context: Context,
+): TokenUnquoted;
+function expectToken(
+  tokens: TokenStream,
+  type: TokenType.Quoted,
+  context: Context,
+): TokenQuoted;
+function expectToken(
+  tokens: TokenStream,
+  type: TokenType,
+  context: Context,
+): Token {
+  const token = tokens.next().value;
+  if (token.type !== type) {
+    context.raise(`Expect token '${TokenType[type]}'`, token);
   }
 
-  const sheet: CueSheeet = {};
+  return token;
+}
 
-  return sheet;
+interface ParserOptions {
+  fatal?: boolean;
+}
+
+export function parse(source: string, options: ParserOptions = {}): CueSheeet {
+  const tokens = tokenize(stripBOM(source));
+
+  const errors: Array<{ message: string; line: number; column: number }> = [];
+  const raise: Context["raise"] = options.fatal
+    ? ((message, errorAt) => {
+      throw new Error(`${message} (${errorAt.line}:${errorAt.column})`);
+    })
+    : ((message, errorAt) => {
+      errors.push({ message, line: errorAt.line, column: errorAt.column });
+    });
+
+  const context: Context = {
+    sheet: {},
+    state: {
+      inTrack: false,
+    },
+    raise,
+  };
+
+  while (true) {
+    const command = parseCommand(tokens, context);
+    context.sheet = { ...context.sheet, ...command };
+
+    const nextToken = tokens.next();
+    if (nextToken.value.type === TokenType.EOF || nextToken.done) {
+      break;
+    } else if (nextToken.value.type !== TokenType.LineBreak) {
+      raise("Expect line break or end of file.", nextToken.value);
+    }
+  }
+
+  return context.sheet;
+}
+
+function parseCommand(tokens: TokenStream, context: Context) {
+  const tokenCommandName = expectToken(tokens, TokenType.Unquoted, context);
+  switch (tokenCommandName.text.toUpperCase()) {
+    case "CATALOG":
+      return parseCatalog(tokens, context);
+  }
+}
+
+const RE_CATALOG = /^\d{13}$/;
+
+function parseCatalog(tokens: TokenStream, context: Context) {
+  const tokenCatalog = expectToken(tokens, TokenType.Unquoted, context);
+
+  if (!RE_CATALOG.test(tokenCatalog.text)) {
+    context.raise("Catalog must be 13 digits.", tokenCatalog);
+  }
+
+  if (context.sheet.catalog) {
+    context.raise("Catalog can appear only once in one file.", tokenCatalog);
+  }
+
+  return { catalog: tokenCatalog.text };
 }
