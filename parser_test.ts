4ba0a3fb56dca11a928589fbd2858deaf76f592e
Pig Fang <g-plane@hotmail.com>
2021-08-16 14:54:01 +0800

    add tests

---
+++ parser_test.ts
@@ -0,0 +1,26 @@
+import { assertEquals } from "https://deno.land/std@0.104.0/testing/asserts.ts";
+import { ErrorKind } from "./errors.ts";
+import { parse } from "./parser.ts";
+
+Deno.test("invalid catalog format", () => {
+  assertEquals(parse(`CATALOG abcdefghijklm`), {
+    sheet: { catalog: "abcdefghijklm" },
+    errors: [{ kind: ErrorKind.InvalidCatalogFormat, line: 1, column: 9 }],
+  });
+
+  assertEquals(parse(`CATALOG 1234`), {
+    sheet: { catalog: "1234" },
+    errors: [{ kind: ErrorKind.InvalidCatalogFormat, line: 1, column: 9 }],
+  });
+});
+
+Deno.test("duplicated catalog", () => {
+  const source = `CATALOG 1234567890123
+CATALOG 0987654321098
+`;
+
+  assertEquals(parse(source), {
+    sheet: { catalog: "0987654321098" },
+    errors: [{ kind: ErrorKind.DuplicatedCatalog, line: 2, column: 9 }],
+  });
+});
